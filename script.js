d5b767c82321fea79b7592976360c3fe6c9f8f8a
Ujjainpujan <bairagivishal333@gmail.com>
2026-07-10 18:12:58 +0530

    Final Premium Website

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // Mahadev Anusthan Kendra
 console.log("Website Loaded Successfully");
+// Mahadev Anusthan Kendra
+
+window.addEventListener("load", function () {
+    document.body.classList.add("loaded");
+});
+
+window.addEventListener("scroll", function () {
+    const header = document.querySelector(".header");
+
+    if (window.scrollY > 50) {
+        header.style.background = "rgba(0,0,0,0.95)";
+    } else {
+        header.style.background = "rgba(0,0,0,0.85)";
+    }
+});
