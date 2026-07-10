3e05dab3b530cd8ad2cd5a0a469119075fac8d77
Ujjainpujan <bairagivishal333@gmail.com>
2026-07-10 20:14:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-// Mahadev Anusthan Kendra
-console.log("Website Loaded Successfully");
-// Mahadev Anusthan Kendra
+window.addEventListener("scroll",function(){
 
-window.addEventListener("load", function () {
-    document.body.classList.add("loaded");
-});
+const header=document.querySelector(".header");
 
-window.addEventListener("scroll", function () {
-    const header = document.querySelector(".header");
+if(window.scrollY>50){
 
-    if (window.scrollY > 50) {
-        header.style.background = "rgba(0,0,0,0.95)";
-    } else {
-        header.style.background = "rgba(0,0,0,0.85)";
-    }
+header.style.background="rgba(0,0,0,.95)";
+
+}else{
+
+header.style.background="rgba(0,0,0,.85)";
+
+}
+
 });
